77c19bb53d5270d4e30ccb617190bc5ccf436e5f
Younggi Min <younggimin91@gmail.com>
2016-05-25 16:13:55 +0900

    post_new with modelform

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^blog/$', 'blog.views.post_list', name='post_list'),
     url(r'^blog/(?P<pk>\d+)/$', 'blog.views.post_detail', name='post_detail'),
+    url(r'^blog/new/$', 'blog.views.post_new', name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     post_list = Post.objects.all()
@@ -11,4 +12,16 @@ def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {
         'post' : post,
+    })
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog.views.post_list')
+    else :
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {
+        'form' : form,
     })
